fee665f67dcc180a2efc03bb42c6d05550216adf
csenet <admin@k1h.dev>
2025-08-13 17:23:09 +0900

    Add TCP socket support with new system calls
    This commit introduces TCP socket support to the kernel, adding new system calls for socket operations including tcp_open, tcp_connect, tcp_bind, tcp_listen, and tcp_accept. It modifies the tcp_pcb structure to accommodate socket modes and enhances the TCP state management. Additionally, the tcp.h header is updated to declare the new functions, improving the kernel's networking capabilities.

--- kernel/net/tcp.h
+++ kernel/net/tcp.h
@@ -8,8 +8,21 @@
 extern int
 tcp_init(void);
 
+extern void
+tcp_dump(const uint8_t *data, size_t len);
+
 extern int
 tcp_open_rfc793(struct ip_endpoint *local, struct ip_endpoint *foreign, int active);
+extern int
+tcp_open(void);
+extern int
+tcp_connect(int id, struct ip_endpoint *foreign);
+extern int
+tcp_bind(int id, struct ip_endpoint *local);
+extern int
+tcp_listen(int id, int backlog);
+extern int
+tcp_accept(int id, struct ip_endpoint *foreign);
 extern int
 tcp_close(int id);
 extern ssize_t
