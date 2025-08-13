43117d5f8736ebff2f1a76ba960aea3da0a52a66
csenet <admin@k1h.dev>
2025-08-13 14:27:02 +0900

    Update Makefile and kernel files to integrate network stack
    This commit modifies the Makefile to include network-related object files and updates compiler flags for the kernel. It adds a new function declaration for netinit in defs.h and integrates a call to netinit in the main function, initializing the network stack during kernel startup. Additionally, it cleans up the build process by including dependencies for the new network components.

--- kernel/main.c
+++ kernel/main.c
@@ -28,14 +28,7 @@ int main()
     iinit();         // inode table
     fileinit();      // file table
     virtio_disk_init(); // emulated hard disk
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    printf("tv: {sec: %ld, usec: %ld}\n", tv.tv_sec, tv.tv_usec);
-    struct tm tm;
-    localtime_r(&tv.tv_sec, &tm);
-    printf("%04d/%02d/%02d %02d:%02d:%02d\n",
-      tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-    printf("%ld\n", mktime(&tm));
+    netinit();       // network stack
     userinit();      // first user process
     __sync_synchronize();
     started = 1;
